546891aa9338ed87c952233e4eee58eff6e7574b
qianji.wang <qianji.wang@nowcoder.com>
2022-03-18 16:06:49 +0800

    add

---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/DegradeFlowRuleNacosProvider.java
@@ -0,0 +1,29 @@
+package com.alibaba.csp.sentinel.dashboard.rule;
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.DegradeRuleEntity;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.csp.sentinel.util.StringUtil;
+import com.alibaba.nacos.api.config.ConfigService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("degradeFlowRuleNacosProvider")
+public class DegradeFlowRuleNacosProvider implements DynamicRuleProvider<List<DegradeRuleEntity>> {
+
+  @Autowired
+  private ConfigService configService;
+  @Autowired
+  private Converter<String, List<DegradeRuleEntity>> converter;
+
+  @Override
+  public List<DegradeRuleEntity> getRules(String appName) throws Exception {
+    String rules = configService.getConfig(appName + NacosConfigUtil.DEGRADE_FLOW_DATA_ID_POSTFIX,
+        NacosConfigUtil.GROUP_ID, 3000);
+    if (StringUtil.isEmpty(rules)) {
+      return new ArrayList<>();
+    }
+    return converter.convert(rules);
+  }
+}
---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/DegradeFlowRuleNacosPublisher.java
@@ -0,0 +1,30 @@
+package com.alibaba.csp.sentinel.dashboard.rule;
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.DegradeRuleEntity;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.csp.sentinel.util.AssertUtil;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.ConfigType;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("degradeFlowRuleNacosPublisher")
+public class DegradeFlowRuleNacosPublisher implements
+    DynamicRulePublisher<List<DegradeRuleEntity>> {
+
+  @Autowired
+  private ConfigService configService;
+  @Autowired
+  private Converter<List<DegradeRuleEntity>, String> converter;
+
+  @Override
+  public void publish(String app, List<DegradeRuleEntity> rules) throws Exception {
+    AssertUtil.notEmpty(app, "app name cannot be empty");
+    if (rules == null) {
+      return;
+    }
+    configService.publishConfig(app + NacosConfigUtil.DEGRADE_FLOW_DATA_ID_POSTFIX,
+        NacosConfigUtil.GROUP_ID, converter.convert(rules), ConfigType.JSON.getType());
+  }
+}
