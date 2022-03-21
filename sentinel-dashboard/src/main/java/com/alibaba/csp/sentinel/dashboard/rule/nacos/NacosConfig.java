6480906d049f348815b65e419e225d10aa247cf8
qianji.wang <qianji.wang@nowcoder.com>
2022-03-21 18:25:43 +0800

    add

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
@@ -15,8 +15,13 @@
  */
 package com.alibaba.csp.sentinel.dashboard.rule.nacos;
 
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.gateway.ApiDefinitionEntity;
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.gateway.GatewayFlowRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.AuthorityRuleEntity;
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.DegradeRuleEntity;
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.ParamFlowRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.SystemRuleEntity;
 import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.api.PropertyKeyConst;
@@ -35,7 +40,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NacosConfig {
 
-  //
   @Value("${nacos.serverAddr}")
   private String serverAddr;
   @Value("${nacos.namespace}")
@@ -45,27 +49,14 @@ public class NacosConfig {
   @Value("${nacos.password}")
   private String password;
 
-  //
   @Bean
   public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
     return JSON::toJSONString;
   }
 
-  //
   @Bean
   public Converter<String, List<FlowRuleEntity>> flowRuleEntityDecoder() {
     return s -> JSON.parseArray(s, FlowRuleEntity.class);
-  }
-
-  // Nacos
-  @Bean
-  public ConfigService nacosConfigService() throws Exception {
-    Properties properties = new Properties();
-    properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
-    properties.put(PropertyKeyConst.NAMESPACE, namespace);
-    properties.put(PropertyKeyConst.USERNAME, username);
-    properties.put(PropertyKeyConst.PASSWORD, password);
-    return ConfigFactory.createConfigService(properties);
   }
 
   @Bean
@@ -77,4 +68,65 @@ public class NacosConfig {
   public Converter<String, List<DegradeRuleEntity>> degradeFlowRuleEntityDecoder() {
     return s -> JSON.parseArray(s, DegradeRuleEntity.class);
   }
+
+  @Bean
+  public Converter<List<ParamFlowRuleEntity>, String> paramFlowRuleEntityEncoder() {
+    return JSON::toJSONString;
+  }
+
+  @Bean
+  public Converter<String, List<ParamFlowRuleEntity>> paramFlowRuleEntityDecoder() {
+    return s -> JSON.parseArray(s, ParamFlowRuleEntity.class);
+  }
+
+  @Bean
+  public Converter<List<ApiDefinitionEntity>, String> gatewayApiRuleEntityEncoder() {
+    return JSON::toJSONString;
+  }
+
+  @Bean
+  public Converter<String, List<ApiDefinitionEntity>> gatewayApiRuleEntityDecoder() {
+    return s -> JSON.parseArray(s, ApiDefinitionEntity.class);
+  }
+
+  @Bean
+  public Converter<List<GatewayFlowRuleEntity>, String> gatewayFlowRuleEntityEncoder() {
+    return JSON::toJSONString;
+  }
+
+  @Bean
+  public Converter<String, List<GatewayFlowRuleEntity>> gatewayFlowRuleEntityDecoder() {
+    return s -> JSON.parseArray(s, GatewayFlowRuleEntity.class);
+  }
+
+  @Bean
+  public Converter<List<AuthorityRuleEntity>, String> authorityRuleEntityEncoder() {
+    return JSON::toJSONString;
+  }
+
+  @Bean
+  public Converter<String, List<AuthorityRuleEntity>> authorityFlowRuleEntityDecoder() {
+    return s -> JSON.parseArray(s, AuthorityRuleEntity.class);
+  }
+
+  @Bean
+  public Converter<List<SystemRuleEntity>, String> systemRuleEntityEncoder() {
+    return JSON::toJSONString;
+  }
+
+  @Bean
+  public Converter<String, List<SystemRuleEntity>> systemRuleEntityDecoder() {
+    return s -> JSON.parseArray(s, SystemRuleEntity.class);
+  }
+
+  @Bean
+  public ConfigService nacosConfigService() throws Exception {
+    Properties properties = new Properties();
+    properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+    properties.put(PropertyKeyConst.NAMESPACE, namespace);
+    properties.put(PropertyKeyConst.USERNAME, username);
+    properties.put(PropertyKeyConst.PASSWORD, password);
+    return ConfigFactory.createConfigService(properties);
+  }
+
 }
